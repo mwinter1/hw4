2eff74b35c15c71e60cffa0dcddeff9601f4bb9b
Matt Winter <winter5187@gmail.com>
2021-08-01 21:30:05 -0500

    updated js variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 // Global Variables
+let time = document.getElementById("timer");
+let score = document.querySelector(".display-3");
+let submit = document.getElementById("buttonInitials");
+let input = document.getElementById("inlineFormInput");
+let seconds = 50; 
+let startButton = document.getElementById("startQuiz");
+let cardQuestions = document.getElementById("questions");
+let displayJumbo = document.querySelector(".jumbotron");
+
+let questions = 
+
 
 // Functions
 
